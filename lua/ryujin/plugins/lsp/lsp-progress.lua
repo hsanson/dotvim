694bb37c88e642b5fc17329367e5b56cdb17a6f6
Horacio <900716+hsanson@users.noreply.github.com>
2023-10-10 08:57:19 +0900

    Fix lsp-progress event

--- lua/ryujin/plugins/lsp/lsp-progress.lua
+++ lua/ryujin/plugins/lsp/lsp-progress.lua
@@ -5,8 +5,9 @@ return {
   config = function()
     require('lsp-progress').setup()
 
-    vim.api.nvim_create_autocmd("User LspProgressStatusUpdated", {
+    vim.api.nvim_create_autocmd("User", {
       group = vim.api.nvim_create_augroup("LualineLspProgressGroup", { clear = true }),
+      pattern = 'LspProgressStatusUpdated',
       callback = require("lualine").refresh,
     })
   end
