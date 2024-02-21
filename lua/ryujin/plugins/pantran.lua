7fe85ad67641dbe6e415a7c6426b04c4af860bb8
Horacio Sanson <900716+hsanson@users.noreply.github.com>
2024-02-21 18:37:32 +0900

    Make pantran fully lazy loaded

--- lua/ryujin/plugins/pantran.lua
+++ lua/ryujin/plugins/pantran.lua
@@ -1,5 +1,23 @@
 return {
+  -- Translation plugin that uses google translate.
+  -- Usage:
+  --   <leader>pr + motion   Translate text selected via motion.
+  --   <leader>prr           Translate current line.
+  --   <leader>pr + visual   Translate visually selected text.
+  --
+  -- UI:
+  --   gt   Change target language.
+  --   gs   Change source language
+  --   gy   Yank translation.
+  --   gr   Replace text with translation.
+  --   ga   Append translation.
+  --   ge   Change translation engine
   "potamides/pantran.nvim",
+  keys = {
+    { "<leader>pr" },
+    { "<leader>prr" },
+    { "<leader>pr", mode = "v" }
+  },
   config = function()
     local pantran = require("pantran")
     local opts = {noremap = true, silent = true, expr = true}
