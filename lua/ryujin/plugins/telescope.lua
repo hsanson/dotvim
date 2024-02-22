5a2691a18af177f3161e94cdfd286c814672689e
Horacio Sanson <900716+hsanson@users.noreply.github.com>
2024-02-22 21:01:50 +0900

    Add zoxide telescope plugin

--- lua/ryujin/plugins/telescope.lua
+++ lua/ryujin/plugins/telescope.lua
@@ -3,14 +3,17 @@ return {
   branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    'nvim-lua/popup.nvim',
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons',
+    'jvgrootveld/telescope-zoxide',
     'BurntSushi/ripgrep',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   config = function()
     local telescope = require('telescope')
     local actions = require("telescope.actions")
+    local z_utils = require("telescope._extensions.zoxide.utils")
 
     telescope.setup {
       defaults = {
@@ -34,11 +37,29 @@ return {
           override_file_sorter = true,     -- override the file sorter
           case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
                                            -- the default case_mode is "smart_case"
+        },
+        zoxide = {
+          prompt_title = "[ Walking on the shoulders of TJ ]",
+          mappings = {
+            default = {
+              after_action = function(selection)
+                print("Update to (" .. selection.z_score .. ") " .. selection.path)
+              end
+            },
+            ["<C-s>"] = {
+              before_action = function(selection) print("before C-s") end,
+              action = function(selection)
+                vim.cmd.edit(selection.path)
+              end
+            },
+            ["<C-q>"] = { action = z_utils.create_basic_command("split") },
+          },
         }
       }
     }
 
     telescope.load_extension('fzf')
+    telescope.load_extension('zoxide')
 
     -- Telescope
     local builtin = require('telescope.builtin')
@@ -48,5 +69,6 @@ return {
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     vim.keymap.set('n', '<leader>fn', ':Telescope find_files search_dirs=~/Seafile/Notes<cr>', {})
+    vim.keymap.set("n", "<leader>cd", telescope.extensions.zoxide.list)
   end
 }
