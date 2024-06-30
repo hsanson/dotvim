cd2c1106ff048597440b06f57b22f6f7b23bc11b
Horacio Sanson <900716+hsanson@users.noreply.github.com>
2024-06-30 20:06:57 +0900

    Replace rest.nvim with kulala.nvim

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -1,19 +1,27 @@
-vim.keymap.set('n', '<localleader>rr', '<cmd>Rest run<cr>', {
-  desc = 'Execute REST request',
+vim.keymap.set('n', '<localleader>rr', function()
+    require('kulala').run()
+  end, {
+  desc = 'Execute request under cursor',
   remap = false,
   buffer = 0
 })
 
-vim.keymap.set('n', '<localleader>rl', '<cmd>Rest run last<cr>', {
-  desc = 'Repeat last REST request',
-  remap = false,
-  buffer = 0
-})
+-- vim.keymap.set('n', '<localleader>rr', '<cmd>Rest run<cr>', {
+--   desc = 'Execute REST request',
+--   remap = false,
+--   buffer = 0
+-- })
+
+-- vim.keymap.set('n', '<localleader>rl', '<cmd>Rest run last<cr>', {
+--   desc = 'Repeat last REST request',
+--   remap = false,
+--   buffer = 0
+-- })
 
 vim.keymap.set('n', '<localleader>re', function()
-    require("telescope").extensions.rest.select_env()
+    require("kulala").set_selected_env()
   end, {
-  desc = 'Select REST environment file',
+  desc = 'Select Kulala environment file',
   remap = false,
   buffer = 0
 })
---
+++ lua/ryujin/plugins/kulala.lua
@@ -0,0 +1,19 @@
+return {
+  -- HTTP REST-Client Interface
+  {
+    'mistweaverco/kulala.nvim',
+    ft = "http",
+    config = function()
+      -- Setup is required, even if you don't pass any options
+      require('kulala').setup({
+        -- default_view, body or headers
+        default_view = "body",
+        -- dev, test, prod, can be anything
+        -- see: https://learn.microsoft.com/en-us/aspnet/core/test/http-files?view=aspnetcore-8.0#environment-files
+        default_env = "dev",
+        -- enable/disable debug mode
+        debug = false,
+      })
+    end
+  },
+}
