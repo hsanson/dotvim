ab1c57612d5f359333a2562027ba442f090dd9ea
Horacio Sanson <900716+hsanson@users.noreply.github.com>
2024-07-09 20:07:08 +0900

    Add kulala header toggle keymap

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -6,22 +6,18 @@ vim.keymap.set('n', '<localleader>rr', function()
   buffer = 0
 })
 
--- vim.keymap.set('n', '<localleader>rr', '<cmd>Rest run<cr>', {
---   desc = 'Execute REST request',
---   remap = false,
---   buffer = 0
--- })
-
--- vim.keymap.set('n', '<localleader>rl', '<cmd>Rest run last<cr>', {
---   desc = 'Repeat last REST request',
---   remap = false,
---   buffer = 0
--- })
-
 vim.keymap.set('n', '<localleader>re', function()
     require("kulala").set_selected_env()
   end, {
   desc = 'Select Kulala environment file',
+  remap = false,
+  buffer = 0
+})
+
+vim.keymap.set('n', '<localleader>rh', function()
+    require("kulala").toggle_view()
+  end, {
+  desc = 'Toogle headers and body response views',
   remap = false,
   buffer = 0
 })
