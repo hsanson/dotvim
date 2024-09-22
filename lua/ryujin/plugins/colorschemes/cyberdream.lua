f2f2eaa98985557df56d5b99a266510496486b91
Horacio Sanson <900716+hsanson@users.noreply.github.com>
2024-09-22 16:55:27 +0900

    Add cyberdream colorscheme

---
+++ lua/ryujin/plugins/colorschemes/cyberdream.lua
@@ -0,0 +1,13 @@
+return {
+  'scottmckendry/cyberdream.nvim',
+  lazy = false,
+  priority = 1000,
+  borderless_telescope = false,
+  opts = {
+    transparent = true,
+    italic_comments = true,
+    hide_fillchars = true,
+    terminal_colors = true,
+    borderless_telescope = false
+  }
+}
