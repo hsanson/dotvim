792a53c02c3bf9f4a8a36f1db2634aec441cbef4
Horacio Sanson <900716+hsanson@users.noreply.github.com>
2024-10-30 23:50:43 +0900

    Update usql.nvim keymaps

--- ftplugin/sql.lua
+++ ftplugin/sql.lua
@@ -1,11 +1,3 @@
-vim.keymap.set('n', '<localleader>re', function()
-    require("usql").select_connection()
-  end, { desc = "Usql switch connection", remap = false, buffer = 0 })
-
-vim.keymap.set('n', '<localleader>rr', function()
-    require("usql").run_statement()
-  end, { desc = "Usql execute SQL statement under the cursor", remap = false, buffer = 0 })
-
-vim.keymap.set('n', '<localleader>rf', function()
-    require("usql").run_file()
-  end, { desc = "Usql execute SQL file", remap = false, buffer = 0 })
+vim.keymap.set("n", "<localleader>re", "<Plug>(SelectConnection)")
+vim.keymap.set({"n", "v"}, "<localleader>rr", "<Plug>(ExecuteStatement)")
+vim.keymap.set("n", "<localleader>rf", "<Plug>(ExecuteFile)")
