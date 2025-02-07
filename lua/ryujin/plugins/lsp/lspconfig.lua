f56a752991c016e90e7e7edb4fb70e5eef163b22
Horacio Sanson <horacio@allm.inc>
2025-02-07 18:22:40 +0900

    Enable lsp inlays

--- lua/ryujin/plugins/lsp/lspconfig.lua
+++ lua/ryujin/plugins/lsp/lspconfig.lua
@@ -61,6 +61,13 @@ return {
         keymap.set('n', '<leader>gf', function()
           vim.lsp.buf.format { async = true }
         end, opts)
+
+        -- Enable inlay hints if server supports them
+        local id = vim.tbl_get(ev, 'data', 'client_id')
+        local client = id and vim.lsp.get_client_by_id(id)
+        if client and client.supports_method('textDocument/inlayHint') then
+          vim.lsp.inlay_hint.enable(true, {bufnr = ev.buf})
+        end
       end
     })
 
