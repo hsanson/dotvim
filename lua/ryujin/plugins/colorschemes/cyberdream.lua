e3c48ed1e114cf5a63d1cfd42b8d0e70c6f5030d
Horacio Sanson <horacio@allm.inc>
2025-02-11 09:01:12 +0900

    Fix warning

--- lua/ryujin/plugins/colorschemes/cyberdream.lua
+++ lua/ryujin/plugins/colorschemes/cyberdream.lua
@@ -2,12 +2,11 @@ return {
   'scottmckendry/cyberdream.nvim',
   lazy = false,
   priority = 1000,
-  borderless_telescope = false,
   opts = {
     transparent = true,
     italic_comments = true,
     hide_fillchars = false,
     terminal_colors = true,
-    borderless_telescope = false
+    borderless_pickers = false
   }
 }
