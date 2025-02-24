661adf79fedf27945c20e34db846d0813b497b68
Horacio Sanson <horacio@allm.inc>
2025-02-24 11:25:19 +0900

    Fix on save for go files

--- lua/ryujin/plugins/ale.lua
+++ lua/ryujin/plugins/ale.lua
@@ -30,9 +30,11 @@ return {
     g.ale_sh_bashate_options = '-i E003 --max-lin-length 100'
     g.ale_ruby_rubocop_auto_correct_all = 1
     g.ale_go_golangci_lint_package = 1
+    g.ale_fix_on_save = 1
 
     g.ale_fixers = {
       ["*"] = {'remove_trailing_lines', 'trim_whitespace'},
+      go  = { 'gofmt' },
       bib = {'bibclean'},
       python = {'yapf'},
       openapi = {'prettier'},
