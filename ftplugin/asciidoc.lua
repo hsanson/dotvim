c05994bdebbfcecd6d2946c1c406acda092a33f3
Horacio Sanson <horacio@allm.inc>
2025-05-22 22:07:17 +0900

    Use firefox to preview asciidoc

--- ftplugin/asciidoc.lua
+++ ftplugin/asciidoc.lua
@@ -1,7 +1,7 @@
 local opt = vim.opt_local
 
 opt.autoindent = true
-opt.expandtab  = true
+opt.expandtab = true
 opt.tabstop = 8
 opt.softtabstop = 2
 opt.shiftwidth = 2
@@ -9,9 +9,16 @@ opt.textwidth = 0
 opt.wrap = true
 opt.linebreak = true
 
-vim.keymap.set('n', 'j', 'gj')
-vim.keymap.set('n', 'k', 'gk')
-vim.keymap.set('n', '<localleader>rr', function()
-  require('autosave.actions').buf_enable()
-  vim.cmd("AsciiDocPreview")
-end, { silent = true, desc = "Preview", buffer =true })
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+vim.keymap.set("n", "<localleader>rr", function()
+  require("autosave.actions").buf_enable()
+  local linux_exe = "firefox"
+  local mac_exe = "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome"
+
+  if vim.fn.executable(linux_exe) > 0 then
+    vim.cmd("silent !" .. linux_exe .. " '" .. vim.fn.expand("%:p") .. "'")
+  elseif vim.fn.executable(mac_exe) then
+    vim.cmd("silent !" .. mac_exe .. " '" .. vim.fn.expand("%:p") .. "'")
+  end
+end, { silent = true, desc = "Preview", buffer = true })
--- lua/ryujin/plugins/nvim-asciidoc-preview.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'tigion/nvim-asciidoc-preview',
-  ft = { 'asciidoc' },
-  build = 'cd server && npm install --omit=dev',
-  opts = {
-    -- Add user configuration here
-  },
-}
