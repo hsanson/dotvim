6c1a43ad231d98eccf273156836780b468dceb0b
hsanson <900716+hsanson@users.noreply.github.com>
2025-05-27 21:38:27 +0900

    Update plugins

--- ftplugin/asciidoc.lua
+++ ftplugin/asciidoc.lua
@@ -14,11 +14,10 @@ vim.keymap.set("n", "k", "gk")
 vim.keymap.set("n", "<localleader>rr", function()
   require("autosave.actions").buf_enable()
   local linux_exe = "firefox"
-  local mac_exe = "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome"
 
   if vim.fn.executable(linux_exe) > 0 then
     vim.cmd("silent !" .. linux_exe .. " '" .. vim.fn.expand("%:p") .. "'")
-  elseif vim.fn.executable(mac_exe) then
-    vim.cmd("silent !" .. mac_exe .. " '" .. vim.fn.expand("%:p") .. "'")
+  else
+    vim.print("firefox binary not found, cannot preview asciidoc document")
   end
 end, { silent = true, desc = "Preview", buffer = true })
