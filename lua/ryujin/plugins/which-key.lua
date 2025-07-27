1f12411136b56b9abfb9d8aaeb0147b73d61fd18
Horacio Sanson <horacio.sanson@dena.jp>
2025-07-27 13:01:46 +0900

    chore(which): change preset to helix

--- lua/ryujin/plugins/which-key.lua
+++ lua/ryujin/plugins/which-key.lua
@@ -2,6 +2,7 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
+    preset = "helix",
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
