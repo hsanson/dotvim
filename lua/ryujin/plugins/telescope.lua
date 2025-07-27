0ed63503a7f8b2241e666c8c7d06e34eca849d85
Horacio Sanson <horacio.sanson@dena.jp>
2025-07-27 21:34:31 +0900

    chore(telescope): allow file search and grep of hidden files

--- lua/ryujin/plugins/telescope.lua
+++ lua/ryujin/plugins/telescope.lua
@@ -1,21 +1,21 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-lua/popup.nvim',
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-tree/nvim-web-devicons',
-    'jvgrootveld/telescope-zoxide',
-    'BurntSushi/ripgrep',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    "nvim-lua/plenary.nvim",
+    "nvim-lua/popup.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+    "jvgrootveld/telescope-zoxide",
+    "BurntSushi/ripgrep",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   config = function()
-    local telescope = require('telescope')
+    local telescope = require("telescope")
     local actions = require("telescope.actions")
     local z_utils = require("telescope._extensions.zoxide.utils")
 
-    telescope.setup {
+    telescope.setup({
       defaults = {
         mappings = {
           i = {
@@ -25,21 +25,21 @@ return {
             ["<A-k>"] = actions.move_selection_previous,
             ["<A-j>"] = actions.move_selection_next,
             ["<A-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          }
-        }
+          },
+        },
       },
       pickers = {
         colorscheme = {
-          enable_preview = true
-        }
+          enable_preview = true,
+        },
       },
       extensions = {
         fzf = {
-          fuzzy = true,                    -- False will only do exact matching
-          override_generic_sorter = true,  -- Override the generic sorter
-          override_file_sorter = true,     -- Override the file sorter
-          case_mode = "smart_case",        -- Or "ignore_case" or "respect_case"
-                                           -- The default case_mode is "smart_case"
+          fuzzy = true, -- False will only do exact matching
+          override_generic_sorter = true, -- Override the generic sorter
+          override_file_sorter = true, -- Override the file sorter
+          case_mode = "smart_case", -- Or "ignore_case" or "respect_case"
+          -- The default case_mode is "smart_case"
         },
         zoxide = {
           prompt_title = "[ Walking on the shoulders of TJ ]",
@@ -47,36 +47,50 @@ return {
             default = {
               after_action = function(selection)
                 print("Update to (" .. selection.z_score .. ") " .. selection.path)
-              end
+              end,
             },
             ["<C-s>"] = {
-              before_action = function(_) print("before C-s") end,
+              before_action = function(_)
+                print("before C-s")
+              end,
               action = function(selection)
                 vim.cmd.edit(selection.path)
-              end
+              end,
             },
             ["<C-q>"] = { action = z_utils.create_basic_command("split") },
           },
-        }
-      }
-    }
+        },
+      },
+    })
 
-    telescope.load_extension('fzf')
-    telescope.load_extension('zoxide')
+    telescope.load_extension("fzf")
+    telescope.load_extension("zoxide")
 
     -- Telescope
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>fc', builtin.colorscheme, {})
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    vim.keymap.set('n', '<leader>ft', function()
-      builtin.grep_string({search_dirs={"~/Seafile/Notes/Allm"}, search="\\[\\s*\\]", use_regex=true})
+    local builtin = require("telescope.builtin")
+
+    vim.keymap.set("n", "<leader>fc", builtin.colorscheme, {})
+    vim.keymap.set("n", "<leader>ff", function()
+      builtin.find_files({
+        file_ignore_patterns = { "^node_modules/", "^.git/", "^.venv/", "^.gradle/", "^vendor/" },
+        hidden = true,
+      })
     end, {})
-    vim.keymap.set('n', '<leader>fn', function()
-      builtin.find_files({search_dirs={"~/Seafile/Notes"}})
+    vim.keymap.set("n", "<leader>fg", function()
+      builtin.live_grep({
+        additional_args = function(opts)
+          return { "--hidden", "--glob", "!**/node_modules/*", "--glob", "!**/.git/*", "--glob", "!**/.gradle/*" }
+        end,
+      })
+    end, {})
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    vim.keymap.set("n", "<leader>ft", function()
+      builtin.grep_string({ search_dirs = { "~/Seafile/Notes/Allm" }, search = "\\[\\s*\\]", use_regex = true })
+    end, {})
+    vim.keymap.set("n", "<leader>fn", function()
+      builtin.find_files({ search_dirs = { "~/Seafile/Notes" } })
     end, {})
     vim.keymap.set("n", "<leader>cd", telescope.extensions.zoxide.list, {})
-  end
+  end,
 }
