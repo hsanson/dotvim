4c19d73e327163c2eca2f4a4a265a3ca64ab0aad
hsanson <900716+hsanson@users.noreply.github.com>
2025-08-28 21:01:39 +0900

    feat: add keymaps for tab navigation

--- lua/ryujin/core/keymaps.lua
+++ lua/ryujin/core/keymaps.lua
@@ -17,6 +17,10 @@ vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Jump below pane" })
 vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Jump above pane" })
 vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Jump right pane" })
 
+-- Tab navigation
+vim.keymap.set("n", "<leader>tn", ":tabnew<cr>", { desc = "Create new tab" })
+vim.keymap.set("n", "<leader>tt", ":tabnext<cr>", { desc = "Next tab" })
+
 -- Remap number increment/decrement so it does not conflict with tmux
 -- control keys (C-a)
 vim.keymap.set("n", "<A-a>", "<C-a>", { desc = "Increment number" })
