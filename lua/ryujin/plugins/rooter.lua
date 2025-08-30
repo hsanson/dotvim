7737c120189ea868dc07aac747570c44e8670bb6
hsanson <900716+hsanson@users.noreply.github.com>
2025-08-30 09:19:53 +0900

    feat: add rooter plugin

---
+++ lua/ryujin/plugins/rooter.lua
@@ -0,0 +1,13 @@
+return {
+  "wsdjeg/rooter.nvim",
+  config = function()
+    require("rooter").setup({
+      root_patterns = { ".git/", "docker-compose.yml", "Dockerfile" },
+      outermost = true,
+      enable_cache = true,
+      project_non_root = "current", -- this can be '', 'home' or 'current'
+      enable_logger = true, -- enable runtime log via logger.nvim
+      command = "tcd", -- cd, tcd or lcd
+    })
+  end,
+}
