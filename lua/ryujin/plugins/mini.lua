10f7e0e22c9d1f596e26614e508711e8cdfb2231
hsanson <900716+hsanson@users.noreply.github.com>
2025-09-21 13:31:52 +0900

    feat: replace treesitter-textobjects with min.ai

---
+++ lua/ryujin/plugins/mini.lua
@@ -0,0 +1,7 @@
+return {
+  "nvim-mini/mini.nvim",
+  version = "*",
+  config = function()
+    require("mini.ai").setup()
+  end,
+}
--- lua/ryujin/plugins/nvim-treesitter.lua
+++ lua/ryujin/plugins/nvim-treesitter.lua
@@ -1,9 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
   config = function()
     local nvim_treesitter = require("nvim-treesitter.configs")
 
@@ -105,47 +102,6 @@ return {
         additional_vim_regex_highlighting = false,
       },
       modules = {},
-      textobjects = {
-        select = {
-          enable = true,
-
-          -- Automatically jump forward to textobj, similar to targets.vim
-          lookahead = true,
-
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["as"] = { query = "@scope", query_group = "locals" },
-            ["ab"] = "@block.outer",
-            ["ib"] = "@block.inner",
-          },
-          -- You can choose the select mode (default is charwise 'v')
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * method: eg 'v' or 'o'
-          -- and should return the mode ('v', 'V', or '<c-v>') or a table
-          -- mapping query_strings to modes.
-          selection_modes = {
-            ["@parameter.outer"] = "v", -- charwise
-            ["@function.outer"] = "V", -- linewise
-            ["@class.outer"] = "<c-v>", -- blockwise
-          },
-          -- If you set this to `true` (default is `false`) then any textobject is
-          -- extended to include preceding or succeeding whitespace. Succeeding
-          -- whitespace has priority in order to act similarly to eg the built-in
-          -- `ap`.
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * selection_mode: eg 'v'
-          -- and should return true of false
-          include_surrounding_whitespace = false,
-        },
-      },
     })
   end,
 }
