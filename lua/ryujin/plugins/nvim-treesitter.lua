e8751eca54cb03fb00acc155b48fe82486593f40
hsanson <900716+hsanson@users.noreply.github.com>
2025-10-11 21:56:57 +0900

    feat: add asciidoc tree-sitter parser

--- lua/ryujin/plugins/nvim-treesitter.lua
+++ lua/ryujin/plugins/nvim-treesitter.lua
@@ -1,8 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "cathaysia/tree-sitter-asciidoc",
+  },
   build = ":TSUpdate",
   config = function()
     local nvim_treesitter = require("nvim-treesitter.configs")
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
     nvim_treesitter.setup({
       ensure_installed = {
@@ -103,5 +107,24 @@ return {
       },
       modules = {},
     })
+
+    parser_config.asciidoc = {
+      install_info = {
+        url = "https://github.com/cathaysia/tree-sitter-asciidoc.git",
+        files = { "tree-sitter-asciidoc/src/parser.c", "tree-sitter-asciidoc/src/scanner.c" },
+        branch = "master",
+        generate_requires_npm = false,
+        requires_generate_from_grammar = false,
+      },
+    }
+    parser_config.asciidoc_inline = {
+      install_info = {
+        url = "https://github.com/cathaysia/tree-sitter-asciidoc.git",
+        files = { "tree-sitter-asciidoc_inline/src/parser.c", "tree-sitter-asciidoc_inline/src/scanner.c" },
+        branch = "master",
+        generate_requires_npm = false,
+        requires_generate_from_grammar = false,
+      },
+    }
   end,
 }
