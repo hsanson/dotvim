ecc5431032e6b5a4dc615161b7703fd2ee0ee80a
hsanson <900716+hsanson@users.noreply.github.com>
2025-10-15 11:07:55 +0900

    chore: improve markdown rendering

--- lua/ryujin/plugins/render-markdown.lua
+++ lua/ryujin/plugins/render-markdown.lua
@@ -1,9 +1,42 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  opts = {
-    file_types = {
-      "markdown",
-    },
-  },
   ft = { "markdown" },
+  config = function()
+    require("render-markdown").setup({
+      file_types = {
+        "markdown",
+      },
+      heading = {
+        icons = { "󰬺 ", "󰬻 ", "󰬼 ", "󰬽 ", "󰬾 ", "󰬿 " },
+      },
+      code = {
+        position = "left",
+        width = "block",
+        left_pad = 1,
+        right_pad = 1,
+        border = "thick",
+      },
+    })
+
+    local bg_alt = "#1e2124"
+    local orange = "#d17c00"
+    local purple = "#a018ff"
+
+    vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH1", { fg = purple })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH2", { fg = purple })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH3", { fg = purple })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH4", { fg = purple })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH5", { fg = purple })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH6", { fg = purple })
+    vim.api.nvim_set_hl(0, "RenderMarkdownCode", { fg = orange, bg = bg_alt, blend = 50 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownCodeInfo", { fg = "#FFFFFF" })
+    vim.api.nvim_set_hl(0, "RenderMarkdownCodeBorder", { bg = bg_alt, blend = 50 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownBullet", { fg = purple })
+  end,
 }
