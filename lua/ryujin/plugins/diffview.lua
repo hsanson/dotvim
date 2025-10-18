c9a962355a78e47a62737638ade086582d998242
hsanson <900716+hsanson@users.noreply.github.com>
2025-10-18 13:56:31 +0900

    chore: add diffview toggle keymap and remove mini.diff

--- lua/ryujin/plugins/diffview.lua
+++ lua/ryujin/plugins/diffview.lua
@@ -2,14 +2,36 @@ return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
   lazy = true,
-  config = function()
-    local diff = require("diffview")
-    diff.setup({
+  opts = {
       hooks = {
         view_leave = function(view)
           view:close()
         end,
       },
-    })
-  end,
+      merge_tool = {
+        disable_diagnostics = true,
+      },
+      file_history = {
+        disable_diagnostics = true,
+      }
+  },
+  keys = {
+    {
+      "<leader>dv", function()
+        local tabpage = vim.api.nvim_get_current_tabpage()
+
+        if not tabpage then
+          return
+        end
+
+        local lib = require("diffview.lib")
+
+        if lib.get_current_view() then
+          vim.cmd("DiffviewClose")
+        else
+          vim.cmd("DiffviewOpen")
+        end
+      end, desc = "Open Diffview" },
+
+  }
 }
--- lua/ryujin/plugins/mini.lua
+++ lua/ryujin/plugins/mini.lua
@@ -3,16 +3,7 @@ return {
   version = "*",
   config = function()
     require("mini.ai").setup()
-    require("mini.diff").setup({
-      view = {
-        style = "sign",
-      },
-    })
     require("mini.pairs").setup()
     require("mini.surround").setup()
-
-    vim.keymap.set("n", "<Leader>dv", function()
-      require("mini.diff").toggle_overlay()
-    end, { desc = "Toggle mini diff overlay" })
   end,
 }
