6c84b3cef89979af8a5647dba4554eca8abf096e
hsanson <900716+hsanson@users.noreply.github.com>
2025-10-21 22:23:52 +0900

    chore: replace openapi plugin with navive filetyp.add() method

--- lua/ryujin/core/filetypes.lua
+++ lua/ryujin/core/filetypes.lua
@@ -1,5 +1,14 @@
 vim.filetype.add({
   extension = {
     http = 'http',
+    yaml = function(path, bufnr)
+      local content = vim.api.nvim_buf_get_lines(bufnr, 0, 20, false)
+      for _, line in ipairs(content) do
+        if line:match('openapi') then
+          return 'openapi.yaml'
+        end
+      end
+      return 'yaml'
+    end,
   },
 })
--- lua/ryujin/plugins/init.lua
+++ lua/ryujin/plugins/init.lua
@@ -1,7 +1,6 @@
 return {
   -- Personal Plugins
   { "hsanson/vim-im", dev = true },
-  { "hsanson/vim-openapi", dev = true },
 
   -- Syntax and language support
   { "neomutt/neomutt.vim" },
