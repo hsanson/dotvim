2c573ae1b10245e0c2fbe3930240f865699cf892
hsanson <900716+hsanson@users.noreply.github.com>
2025-11-16 22:17:29 +0900

    ui: change markdown headers and bullets bg color

--- lua/ryujin/plugins/render-markdown.lua
+++ lua/ryujin/plugins/render-markdown.lua
@@ -22,12 +22,12 @@ return {
     local orange = "#d17c00"
     local purple = "#a018ff"
 
-    vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { fg = "#FFFFFF", bg = purple, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { fg = "#FFFFFF", bg = orange, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { fg = "#FFFFFF", bg = orange, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { fg = "#FFFFFF", bg = orange, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { fg = "#FFFFFF", bg = orange, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { fg = "#FFFFFF", bg = orange, blend = 10 })
+    vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { fg = "#FFFFFF", bg = orange, blend = 10 })
     vim.api.nvim_set_hl(0, "RenderMarkdownH1", { fg = purple })
     vim.api.nvim_set_hl(0, "RenderMarkdownH2", { fg = purple })
     vim.api.nvim_set_hl(0, "RenderMarkdownH3", { fg = purple })
@@ -37,6 +37,6 @@ return {
     vim.api.nvim_set_hl(0, "RenderMarkdownCode", { fg = orange, bg = bg_alt, blend = 50 })
     vim.api.nvim_set_hl(0, "RenderMarkdownCodeInfo", { fg = "#FFFFFF" })
     vim.api.nvim_set_hl(0, "RenderMarkdownCodeBorder", { bg = bg_alt, blend = 50 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownBullet", { fg = purple })
+    vim.api.nvim_set_hl(0, "RenderMarkdownBullet", { fg = orange })
   end,
 }
