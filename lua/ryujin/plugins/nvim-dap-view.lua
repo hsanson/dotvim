00259c678ee8937030d4d029406bf4af6353dc21
hsanson <900716+hsanson@users.noreply.github.com>
2025-12-01 14:02:38 +0900

    fix: remove unused configuration

--- lua/ryujin/plugins/nvim-dap-view.lua
+++ lua/ryujin/plugins/nvim-dap-view.lua
@@ -15,65 +15,41 @@ return {
                 keymap = "B",
                 label = "Breakpoints [B]",
                 short_label = " [B]",
-                action = function()
-                    require("dap-view.views").switch_to_view("breakpoints")
-                end,
             },
             scopes = {
                 keymap = "S",
                 label = "Scopes [S]",
                 short_label = "󰂥 [S]",
-                action = function()
-                    require("dap-view.views").switch_to_view("scopes")
-                end,
             },
             exceptions = {
                 keymap = "E",
                 label = "Exceptions [E]",
                 short_label = "󰢃 [E]",
-                action = function()
-                    require("dap-view.views").switch_to_view("exceptions")
-                end,
             },
             watches = {
                 keymap = "W",
                 label = "Watches [W]",
                 short_label = "󰛐 [W]",
-                action = function()
-                    require("dap-view.views").switch_to_view("watches")
-                end,
             },
             threads = {
                 keymap = "T",
                 label = "Threads [T]",
                 short_label = "󱉯 [T]",
-                action = function()
-                    require("dap-view.views").switch_to_view("threads")
-                end,
             },
             repl = {
                 keymap = "R",
                 label = "REPL [R]",
                 short_label = "󰯃 [R]",
-                action = function()
-                    require("dap-view.repl").show()
-                end,
             },
             sessions = {
                 keymap = "K", -- I ran out of mnemonics
                 label = "Sessions [K]",
                 short_label = " [K]",
-                action = function()
-                    require("dap-view.views").switch_to_view("sessions")
-                end,
             },
             console = {
                 keymap = "C",
                 label = "Console [C]",
                 short_label = "󰆍 [C]",
-                action = function()
-                    require("dap-view.views").switch_to_view("console")
-                end,
             },
         },
         -- Add your own sections
