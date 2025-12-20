1e2bca050e19fe735b61a3dd4acf07fe71b966ab
hsanson <900716+hsanson@users.noreply.github.com>
2025-12-20 12:23:32 +0900

    fix: disable inlay hits that throw many disruptive errors

--- lua/ryujin/plugins/lsp/lspconfig.lua
+++ lua/ryujin/plugins/lsp/lspconfig.lua
@@ -54,9 +54,11 @@ return {
         -- Enable inlay hints if server supports them
         local id = vim.tbl_get(ev, "data", "client_id")
         local client = id and vim.lsp.get_client_by_id(id)
-        if client and client:supports_method("textDocument/inlayHint") then
-          vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
-        end
+
+        -- Inlay hits throw many disrupting errors.
+        -- if client and client:supports_method("textDocument/inlayHint") then
+        --   vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
+        -- end
       end,
     })
 
