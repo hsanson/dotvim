b1ba0816de95b377a544d0f5ca17b8c46f444c68
hsanson <900716+hsanson@users.noreply.github.com>
2026-01-17 17:01:43 +0900

    feat: let render-markdown select its colors based on current scheme

--- lua/ryujin/plugins/render-markdown.lua
+++ lua/ryujin/plugins/render-markdown.lua
@@ -23,27 +23,5 @@ return {
         border = "thick",
       },
     })
-
-    local headingbg = "#2c50a3"
-    local bg_alt = "#1e2124"
-    local orange = "#d17c00"
-    local purple = "#a018ff"
-
-    vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { fg = "#FFFFFF", bg = headingbg, blend = 10 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { fg = "#FFFFFF", bg = headingbg, blend = 10 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { fg = "#FFFFFF", bg = headingbg, blend = 10 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { fg = "#FFFFFF", bg = headingbg, blend = 10 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { fg = "#FFFFFF", bg = headingbg, blend = 10 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { fg = "#FFFFFF", bg = headingbg, blend = 10 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH1", { fg = purple })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH2", { fg = purple })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH3", { fg = purple })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH4", { fg = purple })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH5", { fg = purple })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH6", { fg = purple })
-    vim.api.nvim_set_hl(0, "RenderMarkdownCode", { fg = orange, bg = bg_alt, blend = 50 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownCodeInfo", { fg = "#FFFFFF" })
-    vim.api.nvim_set_hl(0, "RenderMarkdownCodeBorder", { bg = bg_alt, blend = 50 })
-    vim.api.nvim_set_hl(0, "RenderMarkdownBullet", { fg = headingbg })
   end,
 }
