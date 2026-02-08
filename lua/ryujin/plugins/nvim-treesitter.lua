028a41ada11530676a892df7cc9fd0d63758f64f
hsanson <900716+hsanson@users.noreply.github.com>
2026-02-08 18:35:30 +0900

    chore: properly configure treesitter and textobjects

--- lua/ryujin/plugins/nvim-treesitter.lua
+++ lua/ryujin/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  -- Switch to main branch for the new architecture
+  lazy = false,
+  build = ":TSUpdate",
   branch = "main",
   dependencies = {
     "cathaysia/tree-sitter-asciidoc",
@@ -9,11 +10,48 @@ return {
       branch = "main"
     }
   },
-  build = ":TSUpdate",
   config = function()
     -- New architectural way to require treesitter
     local treesitter = require("nvim-treesitter")
+    local textobjects = require("nvim-treesitter-textobjects")
     local parser_config = require("nvim-treesitter.parsers")
+
+    -- Install parsers
+    local parsers = {
+      "awk", "bash", "bibtex", "c", "cmake", "cpp", "css", "csv",
+      "dart", "diff", "dockerfile", "git_config", "git_rebase", "gitcommit",
+      "gitignore", "gnuplot", "go", "gomod", "gosum", "gowork", "gpg",
+      "graphql", "groovy", "helm", "html", "http", "hurl", "ini", "java",
+      "javadoc", "javascript", "jinja", "jinja_inline", "jq", "json", "json5",
+      "julia", "kotlin", "latex", "lua", "luadoc", "make",
+      "markdown", "markdown_inline", "mermaid", "muttrc", "php", "po",
+      "promql", "pug", "python", "readline", "regex", "ruby", "rust", "sql",
+      "ssh_config", "swift", "terraform", "tmux", "toml", "tsv", "typescript",
+      "typst", "vala", "vim", "vimdoc", "vue", "xml", "yaml", "zathurarc",
+    }
+
+    for _, parser in ipairs(parsers) do
+      treesitter.install(parser)
+    end
+
+    local patterns = {}
+    for _, parser in ipairs(parsers) do
+      local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+      for _, pp in pairs(parser_patterns) do
+        table.insert(patterns, pp)
+      end
+    end
+
+    vim.treesitter.language.register("groovy", "Jenkinsfile")
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = patterns,
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
 
     -- Custom Parser Configurations
 
@@ -44,54 +82,111 @@ return {
       tier = 2,
     }
 
-    treesitter.setup({
-      -- Cleaned up duplicates from your list
-      ensure_installed = {
-        "awk", "asciidoc", "bash", "bibtex", "c", "cmake", "cpp", "css", "csv",
-        "dart", "diff", "dockerfile", "git_config", "git_rebase", "gitcommit",
-        "gitignore", "gnuplot", "go", "gomod", "gosum", "gowork", "gpg",
-        "graphql", "groovy", "helm", "html", "http", "hurl", "ini", "java",
-        "javadoc", "javascript", "jinja", "jinja_inline", "jq", "json", "json5",
-        "julia", "kotlin", "kulala_http", "latex", "lua", "luadoc", "make",
-        "markdown", "markdown_inline", "mermaid", "muttrc", "php", "po",
-        "promql", "pug", "python", "readline", "regex", "ruby", "rust", "sql",
-        "ssh_config", "swift", "terraform", "tmux", "toml", "tsv", "typescript",
-        "typst", "vala", "vim", "vimdoc", "vue", "xml", "yaml", "zathurarc",
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
+    textobjects.setup({
+      select = {
         enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<cr>",
-          node_incremental = "<cr>",
-          node_decremental = "<s-cr>",
+        lookahead = true,
+        selection_modes = {
+          ['@parameter.outer'] = 'v',
+          ['@function.outer'] = 'V',
+          ['@class.outer'] = '<c-v>',
         },
+        include_surrounding_whitespace = false,
       },
-      -- Textobjects still works via setup if the plugin is loaded
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-          },
-          selection_modes = {
-            ['@parameter.outer'] = 'v',
-            ['@function.outer'] = 'V',
-            ['@class.outer'] = '<c-v>',
-          },
-          include_surrounding_whitespace = true,
-        },
+      move = {
+        set_jumps = true,
       },
     })
+
+    -- Selects
+    local select = require "nvim-treesitter-textobjects.select"
+    vim.keymap.set({ "x", "o" }, "af", function()
+      select.select_textobject("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "if", function()
+      select.select_textobject("@function.inner", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "ac", function()
+      select.select_textobject("@class.outer", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "ic", function()
+      select.select_textobject("@class.inner", "textobjects")
+    end)
+    -- You can also use captures from other query groups like `locals.scm`
+    vim.keymap.set({ "x", "o" }, "as", function()
+      select.select_textobject("@local.scope", "locals")
+    end)
+
+    -- Swaps
+    local swap = require("nvim-treesitter-textobjects.swap")
+    vim.keymap.set("n", "<leader>a", function()
+      swap.swap_next "@parameter.inner"
+    end)
+    vim.keymap.set("n", "<leader>A", function()
+      swap.swap_previous "@parameter.outer"
+    end)
+
+
+    local move = require("nvim-treesitter-textobjects.move")
+    vim.keymap.set({ "n", "x", "o" }, "]m", function()
+      move.goto_next_start("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n", "x", "o" }, "]]", function()
+      move.goto_next_start("@class.outer", "textobjects")
+    end)
+    -- You can also pass a list to group multiple queries.
+    vim.keymap.set({ "n", "x", "o" }, "]o", function()
+      move.goto_next_start({ "@loop.inner", "@loop.outer" }, "textobjects")
+    end)
+    -- You can also use captures from other query groups like `locals.scm` or `folds.scm`
+    vim.keymap.set({ "n", "x", "o" }, "]s", function()
+      move.goto_next_start("@local.scope", "locals")
+    end)
+    vim.keymap.set({ "n", "x", "o" }, "]z", function()
+      move.goto_next_start("@fold", "folds")
+    end)
+
+    vim.keymap.set({ "n", "x", "o" }, "]M", function()
+      move.goto_next_end("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n", "x", "o" }, "][", function()
+      move.goto_next_end("@class.outer", "textobjects")
+    end)
+
+    vim.keymap.set({ "n", "x", "o" }, "[m", function()
+      move.goto_previous_start("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n", "x", "o" }, "[[", function()
+      move.goto_previous_start("@class.outer", "textobjects")
+    end)
+
+    vim.keymap.set({ "n", "x", "o" }, "[M", function()
+      move.goto_previous_end("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n", "x", "o" }, "[]", function()
+      move.goto_previous_end("@class.outer", "textobjects")
+    end)
+
+    -- Go to either the start or the end, whichever is closer.
+    -- Use if you want more granular movements
+    vim.keymap.set({ "n", "x", "o" }, "]d", function()
+      move.goto_next("@conditional.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n", "x", "o" }, "[d", function()
+      move.goto_previous("@conditional.outer", "textobjects")
+    end)
+
+    local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
+
+    -- Repeat movement with ; and ,
+    -- ensure ; goes forward and , goes backward regardless of the last direction
+    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+
+    -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
   end,
 }
