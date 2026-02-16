da9659f9631f39d6539ae1533ae755612d0cdff9
hsanson <900716+hsanson@users.noreply.github.com>
2026-02-16 13:49:36 +0900

    feat: replace ufo with origami

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -1,4 +1,1 @@
 local opt = vim.opt_local
-
-opt.foldmethod = "expr"
-opt.foldexpr = "nvim_treesitter#foldexpr()"
---
+++ lua/ryujin/plugins/origami.lua
@@ -0,0 +1,11 @@
+return {
+  "chrisgrieser/nvim-origami",
+  event = "VeryLazy",
+  opts = {}, -- required even when using default config
+
+  -- recommended: disable vim's auto-folding
+  init = function()
+    vim.opt.foldlevel = 99
+    vim.opt.foldlevelstart = 99
+  end,
+}
