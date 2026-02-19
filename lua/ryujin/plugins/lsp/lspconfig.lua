932431d16a75442514e9c2f92a5699526d428d1f
hsanson <900716+hsanson@users.noreply.github.com>
2026-02-19 09:21:40 +0900

    chore: properly configure harper ls

--- lua/ryujin/plugins/lsp/lspconfig.lua
+++ lua/ryujin/plugins/lsp/lspconfig.lua
@@ -204,21 +204,14 @@ return {
       settings = {
         ["harper-ls"] = {
           linters = {
-            isolateEnglish = true,
-            spell_check = true,
-            spelled_numbers = false,
-            an_a = true,
-            sentence_capitalization = true,
-            unclosed_quotes = true,
-            wrong_quotes = false,
-            long_sentences = true,
-            repeated_words = true,
-            spaces = true,
-            matcher = true,
-            correct_number_suffix = true,
-            number_suffix_capitalization = true,
-            multiple_sequential_pronouns = true,
+            LongSentences = false,
           },
+          codeActions = {
+            ForceStable = true,
+          },
+          markdown = {
+            isolateEnglish = true,
+          }
         },
       },
     })
