5c803c41e011c82355ecc516b6d9df9ec4b92bce
hsanson <900716+hsanson@users.noreply.github.com>
2026-03-11 20:15:39 +0900

    feat: add usql ftplugin

---
+++ ftplugin/usql.lua
@@ -0,0 +1,45 @@
+local function open_repl()
+  local yarepl = require("yarepl")
+
+  local current_buffer = vim.api.nvim_get_current_buf()
+  local repl = yarepl._get_repl(0, nil, current_buffer)
+
+  if not repl then
+    local current_win = vim.api.nvim_get_current_win()
+    vim.cmd("REPLStart usql")
+    vim.api.nvim_set_current_win(current_win)
+  end
+end
+
+vim.keymap.set("n", "<localleader>rt", function()
+  open_repl()
+end, {desc = "Open SQL REPL"})
+
+vim.keymap.set("n", "<localleader>re", function()
+  open_repl()
+  require("usql").select_connection()
+end, {desc = "Switch DB connection"})
+
+vim.keymap.set("n", "<localleader>rr", function()
+  open_repl()
+  local usql_yarepl = require("usql.yarepl")
+  usql_yarepl.send_statement()
+end, { desc = "Send SQL Statement" })
+
+vim.keymap.set("n", "<localleader>rf", function()
+  open_repl()
+  local usql_yarepl = require("usql.yarepl")
+  usql_yarepl.send_buffer()
+end, { desc = "Send SQL file" })
+
+vim.keymap.set("v", "<localleader>rr", function()
+  local usql_yarepl = require("usql.yarepl")
+  local count = vim.v.count
+  usql_yarepl.send_visual({count = count, args = ""})
+end, {
+  desc = "Send visual region",
+})
+
+vim.keymap.set("n", "<localleader>rc", "REPLCleanup<CR>", {
+  desc = "Clear REPLs.",
+})
