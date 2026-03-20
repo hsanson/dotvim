a0e11aa1ef02ef44e7b3ea2c49ac9a2005c80c74
hsanson <900716+hsanson@users.noreply.github.com>
2026-03-20 12:40:41 +0900

    feat: add smart debug keymap for go files

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -3,6 +3,17 @@ opt.expandtab = false
 
 local opts = { noremap = true, silent = true, buffer = true }
 
+local function smart_debug()
+  local file_name = vim.api.nvim_buf_get_name(0)
+  local is_test_file = file_name:match("_test") or file_name:match(".spec") or file_name:match("test_")
+
+  if is_test_file then
+    require("neotest").run.run({ suite = false, strategy = "dap" })
+  else
+    require("dap").continue()
+  end
+end
+
 opts.desc = "Run test under the cursor"
 vim.keymap.set('n', '<localleader>rr', ':lua require("neotest").run.run()<CR>', opts)
 
@@ -10,7 +21,7 @@ opts.desc = "Run all tests inside current file"
 vim.keymap.set('n', '<localleader>rf', ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>', opts)
 
 opts.desc = "Debug current file"
-vim.keymap.set('n', '<localleader>rd', ':lua require("neotest").run.run({ suite = false, strategy = "dap" })<CR>', opts)
+vim.keymap.set('n', '<localleader>rd', smart_debug, opts)
 
 opts.desc = "Run all test suite"
 vim.keymap.set('n', '<localleader>ra', ':lua require("neotest").run.run({ suite = true })<CR>', opts)
