cb3d9f8beb1cd20b609bb93956540ed0efa21d1f
hsanson <900716+hsanson@users.noreply.github.com>
2026-04-03 22:38:25 +0900

    feat: enable undotree and experimental ui2

--- lua/ryujin/core/options.lua
+++ lua/ryujin/core/options.lua
@@ -140,3 +140,36 @@ opt.formatoptions = "tcrqjmM"
 -- To enable word wrap with line breaks use the textwidth option below.
 -- Note: Use "gq}" to wrap the current paragraph if it is not well formatted.
 opt.textwidth = 80
+
+
+-------------------------------------------------------------------------------
+-- Enable experimental UI2
+-- :h ui2
+require('vim._core.ui2').enable({
+  enable = true, -- Whether to enable or disable the UI.
+  msg = { -- Options related to the message module.
+    ---@type 'cmd'|'msg' Default message target, either in the
+    ---cmdline or in a separate ephemeral message window.
+    ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
+    ---or table mapping |ui-messages| kinds and triggers to a target.
+    targets = 'cmd',
+    cmd = { -- Options related to messages in the cmdline window.
+      height = 0.5 -- Maximum height while expanded for messages beyond 'cmdheight'.
+    },
+    dialog = { -- Options related to dialog window.
+      height = 0.5, -- Maximum height.
+    },
+    msg = { -- Options related to msg window.
+      height = 0.5, -- Maximum height.
+      timeout = 4000, -- Time a message is visible in the message window.
+    },
+    pager = { -- Options related to message window.
+      height = 1, -- Maximum height.
+    },
+  },
+})
+
+-------------------------------------------------------------------------------
+-- Enable undotree
+vim.cmd.packadd("nvim.undotree")
+vim.keymap.set("n", "<leader>u", require("undotree").open)
