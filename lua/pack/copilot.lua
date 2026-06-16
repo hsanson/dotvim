9852862aafb6021f69dff4909443b165aed2443f
hsanson <900716+hsanson@users.noreply.github.com>
2026-06-16 22:05:58 +0900

    feat: replace cmp with blink

--- lua/pack/copilot.lua
+++ lua/pack/copilot.lua
@@ -1,7 +1,7 @@
 vim.pack.add({ 'https://github.com/zbirenbaum/copilot.lua' }, { load = true })
 
 require('copilot').setup({
-  -- Disable suggestions and panel modules to avoid interfering with copilot-cmp
+  -- Disable suggestions and panel modules to avoid interfering with blink.cmp
   suggestion = { enabled = false },
   panel = { enabled = false },
   copilot_model = 'gpt-41-copilot',
--- lua/pack/copilot_cmp.lua
+++
@@ -1,3 +0,0 @@
-vim.pack.add({ 'https://github.com/zbirenbaum/copilot-cmp' }, { load = true })
-
-require('copilot_cmp').setup()
